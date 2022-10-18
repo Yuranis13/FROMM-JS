bed71e52e67f54498b40bce772616385987d50e9
Yuranis13 <yuranis200@gmail.com>
2022-10-18 18:07:01 -0500

    otro poquito

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
 function Carnet( ){
      letname = document.getElementById("Name").value;
+     letSurnames = document.getElementById("Surnames").value;
+     letAge = document.getElementById("Age").value;
+     letPosition = document.getElementById("Position").value;
+     letRH = document.getElementById("RH").value;
+     letDocument = document.getElementById("Document").value;
+     letDocument = document.getElementById("Phone").value;
         
 }
 {
-    console.log(" Name")
+    console.log("Name "),
+    console.log("Surnames "),
+    console.log("Position"),
+    console.log("RH"),
+    console.log("Document"),
+    console.log("Phone")
 }
 
 
